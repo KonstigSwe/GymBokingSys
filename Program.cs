a3d2b88137da7ddff447e36ce7431bf4756a67ec
KonstigSwe <swekonstig@gmail.com>
2021-03-05 11:34:58 +0100

    message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GymBokingSys
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bokking");
         }
     }
 }
