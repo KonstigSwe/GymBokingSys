615d353ba214524c1ae1cd9c4464db2e89a8283a
Johan Thaler <JThaler92@outlook.com>
2021-03-10 15:57:59 +0100

    Schedule class created

--- Schedule.cs
+++ Schedule.cs
@@ -4,19 +4,18 @@ using System.Text;
 
 namespace GymBokingSys
 {
-    class Schedule
+    class TimeSlot
     {
-        public int _timeBooked { get; private set; }
-        public int _duration { get; private set; }
-        public string _bookedBy { get;  private set; }
-        public string employeename { get; private set; }
-        public static List<Schedule> ScheduleList = new List<Schedule>();
-        public Schedule(int timeBooked,int duration, string bookedby, string enploynam)
+        public bool _available;
+        public DateTime _startTime;
+        public int[] _duration = new int[2];
+        public Activity _activity;
+        public TimeSlot(bool available, DateTime startTime, int[] duration, Activity activity)
         {
-            this._timeBooked = timeBooked;
-            this._duration = duration;
-            this._bookedBy = bookedby;
-            this.employeename = enploynam;
+            this._available = available;
+            this._startTime = startTime;
+            this._duration  = duration;
+            this._activity  = activity;
         }
     }
 }
