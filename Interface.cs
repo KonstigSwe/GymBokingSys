4a25b51c95e6e0fbc67d2be9db105744f66a15d2
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2021-03-10 22:55:55 +0100

    New Interface by a little changes.

--- Interface.cs
+++ Interface.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace GymBokingSys
 {
     class Interface
     {
+
         public void welcomeMessage()
         {
-            Console.WriteLine("...Welcome...");
+            Console.WriteLine("╔═══════════════════════════════════════════╗");
+            Console.Write("║");
+            Console.Write("    ...Welcome to Gym booking sysyem...    ");
+            Console.WriteLine("║");
+            Console.WriteLine("╚═══════════════════════════════════════════╝");
+            for (int i = 1; i <= 45; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Thread.Sleep(15);
+                Console.Write("█");
+            }
+            Console.WriteLine();
+            Console.ResetColor();
         }
         public void writeSchedule()
         {
