57539556c9824aea28c833a6646ae13e5565bc64
KonstigSwe <swekonstig@gmail.com>
2021-03-10 09:55:13 +0100

    Created person and inherits !OBS:employee schedule is not linked due to not existing yet

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymBokingSys
+{
+    class User:Person
+    { public User(int phoneNumber,string name,string password):base(phoneNumber, name, password)
+        {
+
+        }
+        public void ShowMyBookings()
+        {
+
+        }
+    }
+}
