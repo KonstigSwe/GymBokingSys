3e0987a2e232c04157a8e021a1b36826e11482a9
Ulrika77 <ulrika.karlsson@yahoo.se>
2021-03-10 10:07:19 +0100

    Add class

---
+++ Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymBokingSys
+{
+    class Group : Activity
+    {
+        private string _groupCoach;
+
+        public string GroupCoach
+        {
+            get { return _groupCoach; }
+            set { _groupCoach = value; }
+        }
+    }
+}
