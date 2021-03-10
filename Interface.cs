bf8387a9a431a2d8004ccdd155ff118870de7e17
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2021-03-10 13:25:19 +0100

    A simple Welcomemessage() methode which can be updated.

--- Interface.cs
+++ Interface.cs
@@ -8,7 +8,20 @@ namespace GymBokingSys
     {
         public void welcomeMessage()
         {
-            Console.WriteLine("...Welcome...");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔══════════════════════════════════════════╗");
+            Console.Write("║");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("   ...Welcome to our booking system...    ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("║");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╚══════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+
+
         }
         public void writeSchedule()
         {
