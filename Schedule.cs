17946a0c304275422c23c7cea6949b336870d9fe
Johan Thaler <JThaler92@outlook.com>
2021-03-11 10:15:31 +0100

    Schedule

--- Schedule.cs
+++ Schedule.cs
@@ -4,18 +4,27 @@ using System.Text;
 
 namespace GymBokingSys
 {
+    class Schedule
+    {
+        public Activity _activity;
+        public List<TimeSlot> _slots = new List<TimeSlot>();
+    }
     class TimeSlot
     {
         public bool _available;
         public DateTime _startTime;
         public int[] _duration = new int[2];
-        public Activity _activity;
-        public TimeSlot(bool available, DateTime startTime, int[] duration, Activity activity)
+        public TimeSlot(bool available, DateTime startTime, int[] duration)
         {
             this._available = available;
             this._startTime = startTime;
             this._duration  = duration;
-            this._activity  = activity;
+        }
+        public TimeSlot()
+        {
+            this._available = true;
+            this._startTime = DateTime.Now;
+            this._duration  = new int[]{ 0, 0 };
         }
     }
 }
