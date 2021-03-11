945a0260b5fdc4b84213f5a3e69ead8ae649a9f6
Johan Thaler <JThaler92@outlook.com>
2021-03-11 13:25:26 +0100

    Schedule updated, Constructor, Default schedule, add slot

--- Schedule.cs
+++ Schedule.cs
@@ -6,20 +6,51 @@ namespace GymBokingSys
 {
     class Schedule
     {
-        public Activity _activity;
-        public List<TimeSlot> _slots = new List<TimeSlot>();
+        public Activity activity;
+        public List<TimeSlot> slots = new List<TimeSlot>();
+        private int[] ts1 = { 8, 0 };
+        private int[] ts2 = { 9, 0 };
+        private int[] ts3 = { 10, 0 };
+        private int[] ts4 = { 11, 0 };
+        private int[] ts5 = { 12, 0 };
+        private int[] ts6 = { 13, 0 };
+        private int[] ts7 = { 14, 0 };
+        private int[] ts8 = { 15, 0 };
+        private int[] ts9 = { 16, 0 };
+        private int[] dur = { 1, 0 };
 
-        public void AddSlot() 
-        { 
-
+        public Schedule(Activity activity)
+        {
+            this.activity = activity;
+            this.slots = new List<TimeSlot>();
         }
+
+        public void NewScheduleDefault(Activity activity)
+        {
+            this.activity = activity;
+            this.slots.Add(new TimeSlot(true, this.ts1,  this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts2, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts3, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts4, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts5, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts6, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts7, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts8, this.dur));
+            this.slots.Add(new TimeSlot(true, this.ts9, this.dur));
+        }
+        public void AddSlot(Activity activity, bool availability, int[] startTime, int[] duration)
+        {
+            this.activity = activity;
+            this.slots.Add(new TimeSlot(availability, startTime, duration));
+        }
+
     }
     class TimeSlot
     {
         public bool _available;
-        public DateTime _startTime;
+        public int[] _startTime = new int[2];
         public int[] _duration = new int[2];
-        public TimeSlot(bool available, DateTime startTime, int[] duration)
+        public TimeSlot(bool available, int[] startTime, int[] duration)
         {
             this._available = available;
             this._startTime = startTime;
@@ -28,7 +59,7 @@ namespace GymBokingSys
         public TimeSlot()
         {
             this._available = true;
-            this._startTime = DateTime.Now;
+            this._startTime = new int[] { 8, 0};
             this._duration  = new int[]{ 0, 0 }; // Hours, minutes
         }
     }
