b0727a4897ef8bc954fc971d9277e0f31a36e038
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2021-03-12 08:51:08 +0100

    The last user inteface for login and register. It is not refactored.

--- Interface.cs
+++ Interface.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace GymBokingSys
 {
@@ -12,24 +13,59 @@ namespace GymBokingSys
             Console.WriteLine("╔══════════════════════════════════════════╗");
             Console.Write("║");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("   ...Welcome to our booking system...    ");
+            Console.Write("   ...Welcome to Gym booking system...    ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("║");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("╚══════════════════════════════════════════╝");
+            Console.ResetColor();
+        }
+
+
+        public void progressBar()
+        {
+            for (int i = 0; i <= 40; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("█");
+                Thread.Sleep(10);
+            }
+            Console.WriteLine();
+        }
+        public void login()
+        {
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Press a key");
-            Console.ReadKey();
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("Log in");
 
 
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(10, 7);
+            Console.Write("Enter your user name: ");
+
+            Console.SetCursorPosition(10, 9);
+            Console.WriteLine("Enter your password: ");
+            Console.ResetColor();
         }
-        public void loginMessage()
+
+        public void loginRegister()
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("Register in the booking system");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(10, 7);
+            Console.Write("Enter your user name: ");
 
-        }
-        public void writeSchedule()
-        {
+            Console.SetCursorPosition(10, 9);
+            Console.WriteLine("Enter your password: ");
 
+            Console.SetCursorPosition(10, 11);
+            Console.WriteLine("Enter your PhoneNumber:");
+            Console.ResetColor();
         }
+      
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace GymBokingSys
     {
         static int level;
         static List<User> UserList = new List<User>();
+
         static void Main(string[] args)
         {
+
 
             XMLreader();
             Interface NewInterface = new Interface();
@@ -18,6 +20,13 @@ namespace GymBokingSys
              level = LoginAndReg();
 
         }
+
+        
+
+
+
+
+
         static void XMLreader()
         {
             
@@ -41,20 +50,43 @@ namespace GymBokingSys
             wfile.Close();
             // Now we can read the serialized book ...  
 
-
-
-
         }
         static void AddAdmin()
         {
             if (level == 10)
             {
-                Console.WriteLine("Enter your username:");
+                //Console.WriteLine("Enter your username:");
+                // var username = Console.ReadLine();
+                //Console.WriteLine("Enter your password:");
+                //var password = Console.ReadLine();
+                //Console.WriteLine("Enter your PhoneNumber:");
+                //string phoneNumber = Console.ReadLine();
+                Interface loginInterface = new Interface();
+                loginInterface.loginRegister();
+                // Creating text boxes
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.SetCursorPosition(32, 7);
+                Console.Write("                              "); // User name
+                Console.SetCursorPosition(32, 9);
+                Console.Write("                              "); // Password
+                Console.SetCursorPosition(34, 11);
+                Console.Write("                              "); // Phone number
+
+                // Reading 
+                Console.SetCursorPosition(32, 7);
                 var username = Console.ReadLine();
-                Console.WriteLine("Enter your password:");
+                Console.SetCursorPosition(32, 9);
                 var password = Console.ReadLine();
-                Console.WriteLine("Enter your PhoneNumber:");
+                Console.SetCursorPosition(34, 11);
                 string phoneNumber = Console.ReadLine();
+                Console.SetCursorPosition(10, 13);
+                loginInterface.progressBar();
+                Console.ResetColor();
+                Console.SetCursorPosition(10, 15);
+                Console.WriteLine("Saved to the bank...");
+                Console.ResetColor();
+                //-----------------------------------------------------
                 User us = new User(phoneNumber, username, password, Role.Admin);
                 UserList.Add(us);
                 XMLwriter();
@@ -64,12 +96,38 @@ namespace GymBokingSys
         {
             if (level == 10)
             {
-                Console.WriteLine("Enter your username:");
+                //Console.WriteLine("Enter your username:");
+                //var username = Console.ReadLine();
+                //Console.WriteLine("Enter your password:");
+                //var password = Console.ReadLine();
+                //Console.WriteLine("Enter your PhoneNumber:");
+                //string phoneNumber = Console.ReadLine();
+                Interface loginInterface = new Interface();
+                loginInterface.loginRegister();
+                // Creating text boxes
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.SetCursorPosition(32, 7);
+                Console.Write("                              "); // User name
+                Console.SetCursorPosition(32, 9);
+                Console.Write("                              "); // Password
+                Console.SetCursorPosition(34, 11);
+                Console.Write("                              "); // Phone number
+
+                // Reading 
+                Console.SetCursorPosition(32, 7);
                 var username = Console.ReadLine();
-                Console.WriteLine("Enter your password:");
+                Console.SetCursorPosition(32, 9);
                 var password = Console.ReadLine();
-                Console.WriteLine("Enter your PhoneNumber:");
+                Console.SetCursorPosition(34, 11);
                 string phoneNumber = Console.ReadLine();
+                Console.SetCursorPosition(10, 13);
+                loginInterface.progressBar();
+                Console.ResetColor();
+                Console.SetCursorPosition(10, 15);
+                Console.WriteLine("Saved to the bank...");
+                Console.ResetColor();
+                //-----------------------------------------------------
                 User us = new User(phoneNumber, username, password, Role.Employee);
                 UserList.Add(us);
                 XMLwriter();
@@ -84,22 +142,49 @@ namespace GymBokingSys
             bool corect = false;
             while (run == 1)
             {
-                Console.WriteLine(" For login press 1 or for register 2");
-     
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(" Select ");
+                Console.WriteLine("[1] Log in    [2] Register");
                 Console.Write(">");
+                Console.ResetColor();
                 var input = Console.ReadLine();
+
 
                 if (input == "1")
                 {
-                    Console.WriteLine("Write your username:");
+                    //Console.WriteLine("Write your username:");
+                    //var username = Console.ReadLine();
+                    //Console.WriteLine("Enter your password:");
+                    //var password = Console.ReadLine();
+                    //-----------------------------------------------------
+                    Interface loginInterface = new Interface();
+                    loginInterface.login();
+                    // Reading user name and password
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.SetCursorPosition(32, 7);
+                    Console.Write("                              "); // User name
+                    Console.SetCursorPosition(32, 9);
+                    Console.Write("                              "); // Password
+
+                    Console.SetCursorPosition(32, 7);
                     var username = Console.ReadLine();
-                    Console.WriteLine("Enter your password:");
+                    Console.SetCursorPosition(32, 9);
                     var password = Console.ReadLine();
-                    
+                    Console.SetCursorPosition(10, 11);
+                    loginInterface.progressBar();
+                    Console.SetCursorPosition(10, 15);
+                    //Console.WriteLine("Thank you");
+                    Console.ResetColor();
+                    //Console.Clear();
+
+                    //-----------------------------------------------------
+
                     foreach (User user in UserList)
                     {
                         if (username == user._name && password == user._password)
-                        { 
+                        {
+                            Console.SetCursorPosition(10, 12);
                             Console.WriteLine("You have successfully logged in !!!");
                             if(user.access == Role.Admin)
                             {
@@ -116,17 +201,45 @@ namespace GymBokingSys
                     }
                     if (corect == false)
                     {
-                        Console.WriteLine("Your username or password is incorect, try again !!!");
+                        Console.SetCursorPosition(10, 12);
+                        Console.WriteLine("Your username or password is incorect, try again !!!\n");
                     }
                 }
                 else if (input == "2")
                 {
-                    Console.WriteLine("Enter your username:");
+                    //Console.WriteLine("Enter your username:");
+                    //var username = Console.ReadLine();
+                    //Console.WriteLine("Enter your password:");
+                    //var password = Console.ReadLine();
+                    //Console.WriteLine("Enter your PhoneNumber:");
+                    //string phoneNumber = Console.ReadLine();
+                    //-----------------------------------------------------
+                    Interface loginInterface = new Interface();
+                    loginInterface.loginRegister();
+                    // Creating text boxes
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.SetCursorPosition(32, 7);
+                    Console.Write("                              "); // User name
+                    Console.SetCursorPosition(32, 9);
+                    Console.Write("                              "); // Password
+                    Console.SetCursorPosition(34, 11);
+                    Console.Write("                              "); // Phone number
+
+                    // Reading 
+                    Console.SetCursorPosition(32, 7);
                     var username = Console.ReadLine();
-                    Console.WriteLine("Enter your password:");
+                    Console.SetCursorPosition(32, 9);
                     var password = Console.ReadLine();
-                    Console.WriteLine("Enter your PhoneNumber:");
+                    Console.SetCursorPosition(34, 11);
                     string phoneNumber = Console.ReadLine();
+                    Console.SetCursorPosition(10, 13);
+                    loginInterface.progressBar();
+                    Console.ResetColor();
+                    Console.SetCursorPosition(10, 15);
+                    Console.WriteLine("Saved to the bank...");
+                    Console.ResetColor();
+                    //-----------------------------------------------------
                     User us = new User(phoneNumber, username, password, Role.User);
                     UserList.Add(us);
                     XMLwriter();
